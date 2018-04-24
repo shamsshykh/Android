48dd203865db8c50d3faa5d39a48d86110e0b17b
Muhammad Shams <shamsshaikh366@gmail.com>
2018-04-24 16:28:26 +0500

    Create third.java

---
+++ app/src/main/java/com/example/mohammadshams/githubdemo/third.java
@@ -0,0 +1,13 @@
+package com.example.mohammadshams.githubdemo;
+
+public class third {
+
+    public void randimangwao(String color,String status,int age,String size){}
+    public void aagae(){}
+    public void paymentkrdo(){}
+    public void tipdydo(){}
+
+    public void finish(){
+
+    }
+}
