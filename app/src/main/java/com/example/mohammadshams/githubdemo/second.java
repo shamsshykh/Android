72a665ef28b1d4caa232b8b10b7e10ca99fdc501
Mohammad.shams <shamsshaikh366@gmail.com>
2018-04-24 16:23:54 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mohammadshams/githubdemo/second.java
@@ -0,0 +1,13 @@
+package com.example.mohammadshams.githubdemo;
+
+public class second {
+
+    public void randimangwao(String color,String status,int age,String size){}
+    public void aagae(){}
+    public void paymentkrdo(){}
+    public void tipdydo(){}
+
+
+
+
+}
