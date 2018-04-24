a5c533cbb415e81817daa34fea2bd9f06f727801
Mohammad.shams <shamsshaikh366@gmail.com>
2018-04-24 16:27:14 +0500

    new Modification

--- app/src/main/java/com/example/mohammadshams/githubdemo/second.java
+++ app/src/main/java/com/example/mohammadshams/githubdemo/second.java
@@ -2,12 +2,8 @@ package com.example.mohammadshams.githubdemo;
 
 public class second {
 
-    public void randimangwao(String color,String status,int age,String size){}
-    public void aagae(){}
-    public void paymentkrdo(){}
-    public void tipdydo(){}
-
-
-
+ public  String newrandi(){
+  return "a";
+ }
 
 }
